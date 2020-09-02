52c5244038fd8f803cfc15d8d5533e2844122e0e
Hannibal Wyman <hannibalwy@gmail.com>
2020-09-02 14:42:28 -0700

    adding MongoAtlas

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const MONGODB_URI = 'mongodb+srv://aychdub:fordtempo@cluster0.evirm.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(MONGODB_URI || "mongodb://localhost/workout", { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+   });
 
 const db = require("./models");
 
